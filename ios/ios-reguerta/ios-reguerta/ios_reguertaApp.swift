bd16a14935254cd09540b75fe402f91abb837f60
Jesús Franco <jesusfrancogarcia@yahoo.es>
2026-02-05 10:52:55 +0100

    Initial commit

---
+++ ios/ios-reguerta/ios-reguerta/ios_reguertaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_reguertaApp.swift
+//  ios-reguerta
+//
+//  Created by Jesús Franco on 05.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct ios_reguertaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/ios-reguerta/ios-reguertaTests/ios_reguertaTests.swift
@@ -0,0 +1,16 @@
+//
+//  ios_reguertaTests.swift
+//  ios-reguertaTests
+//
+//  Created by Jesús Franco on 05.02.2026.
+//
+
+import Testing
+
+struct ios_reguertaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
